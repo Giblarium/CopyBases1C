3bf013eaa463dc2dd3b9003bee9734d9a2926d11
DMITRIY <ilschukdemo@hotmail.com>
2022-01-14 16:35:29 +0300

    Добавьте файлы проекта.

---
+++ CopyBases1C/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CopyBases1C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_ReadBasesList = new System.Windows.Forms.Button();
+            this.button_CopyBases = new System.Windows.Forms.Button();
+            this.textBox_BasesList = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_FolderCopy = new System.Windows.Forms.TextBox();
+            this.listBox_Bases = new System.Windows.Forms.ListBox();
+            this.textBox_debug = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton_NotCopy = new System.Windows.Forms.RadioButton();
+            this.radioButton_Replace = new System.Windows.Forms.RadioButton();
+            this.radioButton_SaveBoth = new System.Windows.Forms.RadioButton();
+            this.button_OpenFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Файл списка баз:";
+            // 
+            // button_ReadBasesList
+            // 
+            this.button_ReadBasesList.Location = new System.Drawing.Point(451, 4);
+            this.button_ReadBasesList.Name = "button_ReadBasesList";
+            this.button_ReadBasesList.Size = new System.Drawing.Size(113, 23);
+            this.button_ReadBasesList.TabIndex = 2;
+            this.button_ReadBasesList.Text = "Обновить список";
+            this.button_ReadBasesList.UseVisualStyleBackColor = true;
+            this.button_ReadBasesList.Click += new System.EventHandler(this.button_ReadBasesList_Click);
+            // 
+            // button_CopyBases
+            // 
+            this.button_CopyBases.Location = new System.Drawing.Point(451, 33);
+            this.button_CopyBases.Name = "button_CopyBases";
+            this.button_CopyBases.Size = new System.Drawing.Size(113, 23);
+            this.button_CopyBases.TabIndex = 3;
+            this.button_CopyBases.Text = "Скопровать базы";
+            this.button_CopyBases.UseVisualStyleBackColor = true;
+            this.button_CopyBases.Click += new System.EventHandler(this.button_CopyBases_Click);
+            // 
+            // textBox_BasesList
+            // 
+            this.textBox_BasesList.Location = new System.Drawing.Point(150, 6);
+            this.textBox_BasesList.Name = "textBox_BasesList";
+            this.textBox_BasesList.Size = new System.Drawing.Size(295, 20);
+            this.textBox_BasesList.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Путь к копиям:";
+            // 
+            // textBox_FolderCopy
+            // 
+            this.textBox_FolderCopy.Location = new System.Drawing.Point(150, 35);
+            this.textBox_FolderCopy.Name = "textBox_FolderCopy";
+            this.textBox_FolderCopy.Size = new System.Drawing.Size(295, 20);
+            this.textBox_FolderCopy.TabIndex = 5;
+            // 
+            // listBox_Bases
+            // 
+            this.listBox_Bases.FormattingEnabled = true;
+            this.listBox_Bases.Items.AddRange(new object[] {
+            "Пусто"});
+            this.listBox_Bases.Location = new System.Drawing.Point(150, 61);
+            this.listBox_Bases.Name = "listBox_Bases";
+            this.listBox_Bases.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox_Bases.Size = new System.Drawing.Size(295, 212);
+            this.listBox_Bases.TabIndex = 0;
+            // 
+            // textBox_debug
+            // 
+            this.textBox_debug.Enabled = false;
+            this.textBox_debug.Location = new System.Drawing.Point(15, 279);
+            this.textBox_debug.Multiline = true;
+            this.textBox_debug.Name = "textBox_debug";
+            this.textBox_debug.Size = new System.Drawing.Size(549, 97);
+            this.textBox_debug.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton_SaveBoth);
+            this.groupBox1.Controls.Add(this.radioButton_Replace);
+            this.groupBox1.Controls.Add(this.radioButton_NotCopy);
+            this.groupBox1.Location = new System.Drawing.Point(452, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 107);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Если копия уже есть:";
+            // 
+            // radioButton_NotCopy
+            // 
+            this.radioButton_NotCopy.AutoSize = true;
+            this.radioButton_NotCopy.Checked = true;
+            this.radioButton_NotCopy.Location = new System.Drawing.Point(7, 33);
+            this.radioButton_NotCopy.Name = "radioButton_NotCopy";
+            this.radioButton_NotCopy.Size = new System.Drawing.Size(101, 17);
+            this.radioButton_NotCopy.TabIndex = 0;
+            this.radioButton_NotCopy.TabStop = true;
+            this.radioButton_NotCopy.Text = "Не копировать";
+            this.radioButton_NotCopy.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Replace
+            // 
+            this.radioButton_Replace.AutoSize = true;
+            this.radioButton_Replace.Location = new System.Drawing.Point(7, 56);
+            this.radioButton_Replace.Name = "radioButton_Replace";
+            this.radioButton_Replace.Size = new System.Drawing.Size(75, 17);
+            this.radioButton_Replace.TabIndex = 0;
+            this.radioButton_Replace.Text = "Заменить";
+            this.radioButton_Replace.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_SaveBoth
+            // 
+            this.radioButton_SaveBoth.AutoSize = true;
+            this.radioButton_SaveBoth.Location = new System.Drawing.Point(6, 79);
+            this.radioButton_SaveBoth.Name = "radioButton_SaveBoth";
+            this.radioButton_SaveBoth.Size = new System.Drawing.Size(99, 17);
+            this.radioButton_SaveBoth.TabIndex = 0;
+            this.radioButton_SaveBoth.Text = "Сохранить оба";
+            this.radioButton_SaveBoth.UseVisualStyleBackColor = true;
+            // 
+            // button_OpenFolder
+            // 
+            this.button_OpenFolder.Location = new System.Drawing.Point(451, 250);
+            this.button_OpenFolder.Name = "button_OpenFolder";
+            this.button_OpenFolder.Size = new System.Drawing.Size(113, 23);
+            this.button_OpenFolder.TabIndex = 3;
+            this.button_OpenFolder.Text = "Открыть копии";
+            this.button_OpenFolder.UseVisualStyleBackColor = true;
+            this.button_OpenFolder.Click += new System.EventHandler(this.button_OpenFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 386);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox_debug);
+            this.Controls.Add(this.textBox_FolderCopy);
+            this.Controls.Add(this.textBox_BasesList);
+            this.Controls.Add(this.button_OpenFolder);
+            this.Controls.Add(this.button_CopyBases);
+            this.Controls.Add(this.button_ReadBasesList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox_Bases);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Копирование баз 1С";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_ReadBasesList;
+        private System.Windows.Forms.Button button_CopyBases;
+        private System.Windows.Forms.TextBox textBox_BasesList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_FolderCopy;
+        private System.Windows.Forms.ListBox listBox_Bases;
+        private System.Windows.Forms.TextBox textBox_debug;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton_SaveBoth;
+        private System.Windows.Forms.RadioButton radioButton_Replace;
+        private System.Windows.Forms.RadioButton radioButton_NotCopy;
+        private System.Windows.Forms.Button button_OpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ CopyBases1C/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CopyBases1C
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_BasesList.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"1C\1CEStart\ibases.v8i"); //todo автовыбор appdata
+            DateTime date = DateTime.Now;
+            string dateStr = date.Year.ToString() + date.Month.ToString("D2") + date.Day.ToString("D2");
+            textBox_FolderCopy.Text = @"D:\BIT\Archive\" + dateStr;
+            //button_ReadBasesList_Click();
+
+
+            
+
+        }
+
+        public class Bases
+        {
+            public string name;
+            public string path;
+            public bool check;
+        }
+
+        List<Bases> listBase = new List<Bases>();
+
+        private void button_ReadBasesList_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_BasesList.Text))
+            {
+                textBox_debug.Text = "Файл списка баз не найден!";
+                return;
+            }
+            StreamReader sr_BasesList = new StreamReader(textBox_BasesList.Text);
+            
+            listBox_Bases.Items.Clear();
+            listBase.Clear();
+
+            string namebase = "", pathbase = "";
+
+            while (!sr_BasesList.EndOfStream)
+            {
+                string str = sr_BasesList.ReadLine();
+                if (str[0] == '[')
+                {
+                    str = str.Remove(0, 1);
+                    str = str.Remove(str.Length - 1, 1);
+                    namebase = str;
+                    listBox_Bases.Items.Add(str);
+
+                }
+                if (str.Contains("Connect="))
+                {
+                    if (str.Contains("File"))
+                    {
+                        str = str.Remove(0, 14);
+                        str = str.Remove(str.Length - 2, 2);
+                        pathbase = str;
+                        listBase.Add(new Bases { name = namebase, path = pathbase });
+                    }
+                    else
+                    {
+                        listBox_Bases.Items.Remove(namebase);
+                        listBox_Bases.Items.Add("[серверная база] " + namebase);
+                        listBase.Add(new Bases { name = namebase, path = pathbase });
+                    }
+
+                }
+                
+            }
+        }
+
+        private void button_CopyBases_Click(object sender, EventArgs e)
+        {
+            if (listBox_Bases.SelectedIndices.Count == 0)
+            {
+                textBox_debug.Text = "Не выбраны базы!";
+                return;
+            }
+            for (int i = 0; i < listBox_Bases.Items.Count; i++)
+            {
+               if (listBox_Bases.GetSelected(i))
+                {
+                    listBase[i].check = true;
+                    CopyBases(listBase[i].name, listBase[i].path);
+                }
+                
+                 
+            }
+
+
+        }
+
+        private void CopyBases(string name, string path)
+        {
+            string sourceFile = Path.Combine(path, "1Cv8.1CD");
+            string copyFile = Path.Combine(textBox_FolderCopy.Text, name + ".1cd");
+            string debugText = "";
+
+            if (!File.Exists(sourceFile))
+            {
+                debugText += "База " + name + " не скопирована, т.к. файл базы данных не найден.\n";
+                return;
+            }
+
+            if (!Directory.Exists(textBox_FolderCopy.Text))
+            {
+                Directory.CreateDirectory(textBox_FolderCopy.Text);
+                debugText = "Создана папка:" + textBox_FolderCopy.Text + "\n";
+
+            }
+            if (File.Exists(copyFile))
+            {
+                if (radioButton_NotCopy.Checked)
+                {
+                    debugText += "База " + name + " не скопирована, т.к. файл уже существует.\n";
+                    return;
+                }
+                else if (radioButton_Replace.Checked)
+                {
+                    File.Copy(sourceFile, copyFile, true);
+                    debugText += "База " + name + " скопирована, файл заменен.\n";
+                }
+                else if (radioButton_SaveBoth.Checked)
+                {
+                    DateTime dateTime = DateTime.Now;
+                    string strDateTime = dateTime.Year.ToString("D4") + dateTime.Month.ToString("D2") + dateTime.Day.ToString("D2") +
+                        dateTime.Hour.ToString("D2") + dateTime.Minute.ToString("D2") + dateTime.Second.ToString("D2");
+                    copyFile = Path.Combine(textBox_FolderCopy.Text, name + strDateTime + ".1cd");
+                    File.Copy(sourceFile, copyFile);
+                    debugText += "База " + name + " скопирована, обе копии сохранинены.\n";
+
+
+                }
+            }
+            else
+            {
+                File.Copy(sourceFile, copyFile);
+                debugText += "База " + name + " скопирована.\n";
+            }
+            textBox_debug.Text = debugText;
+
+            //throw new NotImplementedException();
+        }
+
+        private void button_OpenFolder_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox_FolderCopy.Text))
+            {
+                System.Diagnostics.Process.Start("explorer.exe", textBox_FolderCopy.Text);
+
+            }
+            else
+            {
+                textBox_debug.Text = "Папки копий не существует";
+            }
+        }
+    }
+}
